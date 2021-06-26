065895cf0289680dab1f32e61bdd4b9328b7d6e4
Maous-B <79797065+Maous-B@users.noreply.github.com>
2021-06-26 20:59:41 +0200

    Add files via upload

---
+++ Fonction-polynome-du-second-degre.py
@@ -0,0 +1,23 @@
+﻿#!usr/bin/env python3
+from lycee import *
+from math import *
+import sys
+print("ax² + bx + c")
+a = input("Entrez a :")
+b = input("Entrez b :")
+c = input("Entrez c :")
+
+print(a + "x²" + b + "x" + c)
+delta = int(b)*int(b) - 4 * int(a) * int(c)
+print(delta)
+if delta > 0:
+    print("La fonction admet deux racines")
+    premieresolution = (b-sqrt(delta)) / 2 * a
+    deuxiemesolution = (b+sqrt(delta)) / 2 * a
+    print("x1 = " + premieresolution)
+    print("x2 = " + deuxiemesolution)
+if delta < 0:
+    print("La fonction n'admet aucune racines")
+    sys.exit()
+if delta == 0:
+    print("La fonction admet une racine")
